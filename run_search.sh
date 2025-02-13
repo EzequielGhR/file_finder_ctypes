e780665618fe80a100e3d7f5de9d21d55b8fd76c
EzequielGhR <ezequielghra97@gmail.com>
2025-02-13 00:52:15 -0300

    Fixes for run search script

--- run_search.sh
+++ run_search.sh
@@ -12,19 +12,19 @@ fi
 
 if [ -z "${1}" ] || [ -z "${2}" ]
     then echo "Missing arguments. Usage: ${0} [path] [name | -null] [content]"
-    echo "Help message: ${0} --help"
+    echo "Help message: ${0} -help"
     exit 1
 fi
 
 if [ "${2}" == "-null" ] && [ -z "${3}" ]
     then echo "If name is \"-null\", content must be provided. Usage: ${0} [path] [name | -null ] [content]"
-    echo "Help message: ${0} --help"
+    echo "Help message: ${0} -help"
     exit 1
 fi
 
 if [ "${2}" == "-null" ]
-    cd $SCRIPT_DIR
-    then python main.py --path "${1}" --content "${3}"
+    then cd $SCRIPT_DIR
+    python main.py --path "${1}" --content "${3}"
     cd $EXEC_DIR
     exit 0
 fi
